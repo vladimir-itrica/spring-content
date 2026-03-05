c1b1db9aa48a789ef278cff7d5a2e970588652ed
Vladimir Svyatski <vladimir@itrica.com>
2026-03-05 19:52:24 +0300

    No lambda or function reference
    It just doesn't work at runtime

--- spring-content-fs/src/main/java/internal/org/springframework/content/fs/config/FilesystemStoreConfiguration.java
+++ spring-content-fs/src/main/java/internal/org/springframework/content/fs/config/FilesystemStoreConfiguration.java
@@ -21,7 +21,7 @@ public class FilesystemStoreConfiguration {
     @Bean
     public PlacementService filesystemStorePlacementService() {
         PlacementService conversion = new PlacementServiceImpl();
-        conversion.addConverter((Converter<URI, String>) URI::toString);
+        conversion.addConverter(new URIStringConverter());
 
         addConverters(conversion);
         return conversion;
@@ -33,6 +33,19 @@ public class FilesystemStoreConfiguration {
 
         for (FilesystemStoreConfigurer configurer : configurers) {
             configurer.configureFilesystemStoreConverters(registry);
+        }
+    }
+
+    /**
+     * Important! We have a dedicated class here instead of in-place interface creation (I guess the compiler just
+     * generates similar class on its own). The problem is, certain IDEs will show a warning and offer you to
+     * transform that into a lambda expression. Unfortunately, that causes an exception in runtime. To avoid that
+     * warning from IDEs we just implement the interface ourselves.
+     */
+    private static class URIStringConverter implements Converter<URI, String> {
+        @Override
+        public String convert(URI source) {
+            return source.toString();
         }
     }
 }
