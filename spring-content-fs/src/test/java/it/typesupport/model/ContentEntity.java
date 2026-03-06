481e5298cde3514afafd6f3b8840c86e8a89533b
Vladimir Svyatski <v.svyatski@gmail.com>
2026-03-06 21:55:10 +0300

    We don't need @ContentLength for this test

--- spring-content-fs/src/test/java/it/typesupport/model/ContentEntity.java
+++ spring-content-fs/src/test/java/it/typesupport/model/ContentEntity.java
@@ -1,7 +1,6 @@
 package it.typesupport.model;
 
 import org.springframework.content.commons.annotations.ContentId;
-import org.springframework.content.commons.annotations.ContentLength;
 
 import java.io.Serializable;
 
@@ -9,22 +8,11 @@ public class ContentEntity<SID extends Serializable> {
     @ContentId
     private SID contentId;
 
-    @ContentLength
-    private long contentLength;
-
     public SID getContentId() {
         return contentId;
     }
 
     public void setContentId(SID contentId) {
         this.contentId = contentId;
-    }
-
-    public long getContentLength() {
-        return contentLength;
-    }
-
-    public void setContentLength(long contentLength) {
-        this.contentLength = contentLength;
     }
 }
